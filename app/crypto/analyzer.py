5a9bde1d2561641fb2158db9348c7244f494ff92
avictorchamp <avictorchamp@gmail.com>
2026-08-01 12:18:03 +0700

    Update analyzer.py

--- app/crypto/analyzer.py
+++ app/crypto/analyzer.py
@@ -1,42 +1,79 @@
-def analyze(symbol, price):
+from app.crypto.indicators import (
+    calculate_rsi,
+    calculate_ema,
+    support_resistance
+)
 
-    result = {
-        "coin": symbol,
-        "price": price,
-        "signal": "WAIT"
+from app.crypto.strategy import (
+    generate_signal
+)
+
+
+
+def analyze(
+    coin,
+    data
+):
+
+
+    prices=data["prices"]
+
+
+    price=data["price"]
+
+
+    rsi=calculate_rsi(
+        prices
+    )
+
+
+    ema20=calculate_ema(
+        prices,
+        20
+    )
+
+
+    ema50=calculate_ema(
+        prices,
+        50
+    )
+
+
+    support,resistance = support_resistance(
+        prices
+    )
+
+
+    result=generate_signal(
+        rsi,
+        ema20,
+        ema50
+    )
+
+
+    return {
+
+        "coin":coin,
+
+        "price":price,
+
+        "signal":
+            result["signal"],
+
+        "confidence":
+            result["confidence"],
+
+        "reason":
+            result["reason"],
+
+        "rsi":rsi,
+
+        "ema20":ema20,
+
+        "ema50":ema50,
+
+        "support":support,
+
+        "resistance":resistance
+
     }
-
-
-    if symbol == "XRP":
-
-        support = 2.70
-        resistance = 3.05
-
-
-    elif symbol == "ETH":
-
-        support = 3500
-        resistance = 4200
-
-
-    else:
-
-        support = 0
-        resistance = 0
-
-
-    result["support"] = support
-    result["resistance"] = resistance
-
-
-    if price <= support * 1.02:
-
-        result["signal"] = "BUY WATCH"
-
-
-    elif price >= resistance * 0.98:
-
-        result["signal"] = "SELL WATCH"
-
-
-    return result
